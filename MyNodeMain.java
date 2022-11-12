3c93ffe15a518bdcfb9d58931ee003f4da0ad0ed
mandira-05 <mandira.sn.gowda@gmail.com>
2022-11-12 17:26:38 +0530

    create simple linked list

---
+++ MyNodeMain.java
@@ -0,0 +1,15 @@
+public class MyNodeMain {
+    public static void main(String[] args) {
+
+        MyNode<Integer> firstNode = new MyNode(56);
+        MyNode<Integer> secondNode = new MyNode(30);
+        MyNode<Integer> thirdNode = new MyNode(70);
+
+        firstNode.setNext(secondNode);
+        secondNode.setNext(thirdNode);
+
+        boolean result = firstNode.getNext().equals(secondNode) && secondNode.getNext().equals(thirdNode);
+        System.out.println(result);
+
+    }
+}
