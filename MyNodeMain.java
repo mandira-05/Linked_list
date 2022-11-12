684eb535977896c1097f92019acc625d190875f9
mandira-05 <mandira.sn.gowda@gmail.com>
2022-11-12 17:53:25 +0530

    Added UC2 , interface inode

--- MyNodeMain.java
+++ MyNodeMain.java
@@ -1,15 +1,15 @@
 public class MyNodeMain {
     public static void main(String[] args) {
 
-        MyNode<Integer> firstNode = new MyNode(56);
+        MyNode<Integer> firstNode = new MyNode(70);
         MyNode<Integer> secondNode = new MyNode(30);
-        MyNode<Integer> thirdNode = new MyNode(70);
+        MyNode<Integer> thirdNode = new MyNode(56);
 
-        firstNode.setNext(secondNode);
-        secondNode.setNext(thirdNode);
-
-        boolean result = firstNode.getNext().equals(secondNode) && secondNode.getNext().equals(thirdNode);
-        System.out.println(result);
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(firstNode);
+        linkedList.add(secondNode);
+        linkedList.add(thirdNode);
+        linkedList.printNodes();
 
     }
 }
